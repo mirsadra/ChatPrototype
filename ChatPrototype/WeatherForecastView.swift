3dada66fcd2634c830e37d556f33df639a708e93
Mirsadra Molaei <miirsadra@gmail.com>
2024-10-19 19:56:35 +0330

    isRainy, iconColor compound properties (if statements)

--- ChatPrototype/WeatherForecastView.swift
+++ ChatPrototype/WeatherForecastView.swift
@@ -6,9 +6,9 @@ import SwiftUI
 struct WeatherForecastView: View {
     var body: some View {
         HStack {
-            DayForecast(day: "Mon", high: 70, low: 50)
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
             
-            DayForecast(day: "Tue", high: 60, low: 40)
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
         }
     }
 }
@@ -18,17 +18,27 @@ struct WeatherForecastView: View {
 }
 
 struct DayForecast: View {
-    let day: String     // day is a property of type String
+    let day: String     // day is a stored property of type String
+    let isRainy: Bool
     let high: Int8
     let low: Int8
+    
+    // isRainy property is Bool, so you've to create a compound property (aka: accessor) that represents the icon's name
+    var iconName: String {
+        return isRainy ? "cloud.rain.fill" : "sun.max.fill"
+    }
+    
+    var iconColor: Color {
+        return isRainy ? Color.blue : Color.yellow
+    }
     
     var body: some View {
         VStack {
             Text(day)
-            Image(systemName: "sun.max.fill")
-                .foregroundStyle(Color.yellow)
-            Text("\(high)")
-            Text("\(low)")
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+            Text("High: \(high)ºC")
+            Text("Low: \(low)ºC")
         }
         .padding()
     }
