4441ae166d562d999895ceb4b26be48f047bac24
Mirsadra Molaei <miirsadra@gmail.com>
2024-10-19 19:38:06 +0330

    generalizing high & low temp parameters

--- ChatPrototype/WeatherForecastView.swift
+++ ChatPrototype/WeatherForecastView.swift
@@ -6,9 +6,9 @@ import SwiftUI
 struct WeatherForecastView: View {
     var body: some View {
         HStack {
-            DayForecast(day: "Mon")
+            DayForecast(day: "Mon", high: 70, low: 50)
             
-            DayForecast(day: "Tue")
+            DayForecast(day: "Tue", high: 60, low: 40)
         }
     }
 }
@@ -19,15 +19,16 @@ struct WeatherForecastView: View {
 
 struct DayForecast: View {
     let day: String     // day is a property of type String
-    
+    let high: Int8
+    let low: Int8
     
     var body: some View {
         VStack {
             Text(day)
             Image(systemName: "sun.max.fill")
                 .foregroundStyle(Color.yellow)
-            Text("High: 70")
-            Text("Low: 50")
+            Text("\(high)")
+            Text("\(low)")
         }
         .padding()
     }
