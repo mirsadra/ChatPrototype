d7d602059cd54e149c86a84e3dc3fc1c66569e06
Mirsadra Molaei <miirsadra@gmail.com>
2024-10-19 17:00:50 +0330

    2. Customize views with properties (WeatherForecast)

---
+++ ChatPrototype/WeatherForecastView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherForecastView.swift
+//  ChatPrototype
+//
+//  Created by Mirsadra on 2024. 10. 19..
+//
+
+import SwiftUI
+
+struct WeatherForecastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherForecastView()
+}
