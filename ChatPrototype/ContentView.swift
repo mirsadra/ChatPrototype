52717d99b740b4267aefe01e52f08087eafc4277
Mirsadra Molaei <miirsadra@gmail.com>
2024-10-20 11:56:26 +0330

    kidding

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -7,17 +7,12 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            ZStack {
-                RoundedRectangle(cornerSize: CGSize(width: 40, height: 40))
-                    .fill(Color.teal)
-                    .frame(width: 110, height: 110)
-                Image(systemName: "paperplane.fill")
-                    .foregroundStyle(.background)
-            }
             Text("by Mahsa Pirani")
                 .font(.subheadline)
                 .padding()
                 .background(Color.blue, in: RoundedRectangle(cornerRadius: 8))
+            
+            
         }
         .padding()
     }
