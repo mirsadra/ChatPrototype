745e488efe758c186baf66979efac9218adec97e
Mirsadra Molaei <miirsadra@gmail.com>
2024-10-20 22:11:19 +0330

    design an interface  (layout an interface)

--- ChatPrototype/WelcomePage.swift
+++ ChatPrototype/WelcomePage.swift
@@ -6,10 +6,26 @@ import SwiftUI
 struct WelcomePage: View {
     var body: some View {
         VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(width: 130, height: 130)
+                    .foregroundStyle(.tint)
+                Image(systemName: "sun.rain.fill")
+                    .font(Font.largeTitle)
+                    .foregroundStyle(.blue)
+     
+                // Yellow part (sun)
+                Image(systemName: "sun.rain.fill")
+                    .font(Font.system(size: 70))
+                    .foregroundStyle(.white)
+     
+            }
+            
             Text("Welcome to My App")
                 .font(.largeTitle)
                 .fontWeight(.semibold)
                 .fontWidth(.condensed)
+                .padding(.top)
                 .border(.secondary, width: 1.5)
             Text("Enjoy the suggestions to wear a suitable outfit for this weather")
                 .font(.headline)
