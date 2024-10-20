7566d1bb721d5dbb6c0652f150ad3a3351bc5c6c
Mirsadra Molaei <miirsadra@gmail.com>
2024-10-20 22:14:13 +0330

    Feature Card has bee made

---
+++ ChatPrototype/FeatureCard.swift
@@ -0,0 +1,18 @@
+//
+//  FeatureCard.swift
+//  ChatPrototype
+//
+//  Created by Mirsadra on 2024. 10. 20..
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeatureCard()
+}
