d02260432316889915155922f9ee5d91f6dd7118
Mirsadra Molaei <miirsadra@gmail.com>
2024-10-21 14:10:43 +0330

    combine screens with TabView

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -5,16 +5,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        VStack {
-            Text("by Mahsa Pirani")
-                .font(.subheadline)
-                .padding()
-                .background(Color.blue, in: RoundedRectangle(cornerRadius: 8))
-            
-            
+        TabView {
+            WelcomePage()
+            FeaturesPage()
         }
-        .padding()
+        .tabViewStyle(.page)
     }
 }
 
