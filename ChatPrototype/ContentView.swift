84d12846bb4b7072d08b7ddddebe265b9e3dc402
Mirsadra Molaei <miirsadra@gmail.com>
2024-10-21 14:24:54 +0330

    applying color asset to the content view

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -3,12 +3,19 @@
 
 import SwiftUI
 
+// declare a global property (can be used in all pages), an array of two colors
+let gradientColors: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
 struct ContentView: View {
     var body: some View {
         TabView {
             WelcomePage()
             FeaturesPage()
         }
+        .background(Gradient(colors: gradientColors))
         .tabViewStyle(.page)
     }
 }
