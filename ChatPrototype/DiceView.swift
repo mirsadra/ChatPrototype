7426a3effc4caa245b00dbc354323e96f0040349
Mirsadra Molaei <miirsadra@gmail.com>
2024-10-22 13:46:54 +0330

    update the UI with @State (DiceView)

---
+++ ChatPrototype/DiceView.swift
@@ -0,0 +1,26 @@
+//  DiceView.swift
+//  ChatPrototype
+
+import SwiftUI
+
+struct DiceView: View {
+    @State var numberOfPips: Int = 1
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "die.face.\(numberOfPips)")
+                .resizable()
+                .frame(width: 140, height: 140)
+            Button("Roll") {
+                withAnimation {
+                    numberOfPips = Int.random(in: 1...6)
+                }
+            }
+            .buttonStyle(.bordered)
+        }
+    }
+}
+
+#Preview {
+    DiceView()
+}
